712fd86e82f81bf046d1d18b6fa9fbbc8f1a503a
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-09-03 21:31:21 +0200

    first Lua drawing ( first for this app )

--- lua-scripts/app.lua
+++ lua-scripts/app.lua
@@ -1,9 +1,20 @@
-
 if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
   -- launched through "lldebugger" (ms-vscode)
   require("lldebugger").start()
 end
 
-print(1)
-print(2)
-print(3)
+function DrawRectangle(x, y, w, h)
+  for px = x, x + w - 1 do
+    for py = y, y + h - 1 do
+      DrawPoint(px, py)
+    end
+  end
+end
+
+function Draw()
+  SetDrawColor(0xCC, 0xFF, 0x00)
+  DrawRectangle(20, 30, 31, 31)
+
+  SetDrawColor(0xCC, 0xFF, 0x00)
+  DrawRectangle(60, 30, 31, 31)
+end
